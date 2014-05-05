1ae170024746bc13c91fd48f6a30d8394e293655
trekkie4life <armandoreed@gmail.com>
2014-05-04 17:36:20 -0700

    Add img files, css & jQuery for Parallax

---
+++ js/parallax.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function(){
+	$objWindow = $(window);
+	$('div[data-type="background"]').each(function(){
+		var $bgObj = $(this);
+		$(window).scroll(function() {
+			var yPos = -($objWindow.scrollTop() / $bgObj.data('speed'));
+
+			var coords = '100% '+ yPos + 'px';
+            // Animate the background
+			 $bgObj.css({ backgroundPosition: coords });
+
+		});
+
+	});
+});
