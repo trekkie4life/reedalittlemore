ff320ae5a2b8fed6297365dcb6d03c664a110f85
trekkie4life <armandoreed@gmail.com>
2014-05-17 01:07:01 -0700

    Change background of Projects section on index.html Comment Out parallax to keep page scrolling smooth Edit Index.html to include all project pags

--- js/parallax_scroll.js
+++ js/parallax_scroll.js
@@ -1,16 +1,26 @@
 jQuery(document).ready(function(){
-	$objWindow = $(window);
-	$('div[data-type="background"]').each(function(){
-		var $bgObj = $(this);
-		$(window).scroll(function() {
-			var yPos = -($objWindow.scrollTop() / $bgObj.data('speed'));
+	// $objWindow = $(window);
+	// $('div[data-type="background"]').each(function(){
+	// 	var $bgObj = $(this);
+	// 	$(window).scroll(function() {
+	// 		var yPos = -($objWindow.scrollTop() / $bgObj.data('speed'));
 
-			var coords = '100% '+ yPos + 'px';
-            // Animate the background
-			 $bgObj.css({ backgroundPosition: coords });
+	// 		var coords = '100% '+ yPos + 'px';
+ //            // Animate the background
+	// 		 $bgObj.css({ backgroundPosition: coords });
 
-		});
-	});
+	// 	});
+	// });
 
 	$('#header').localScroll({duration:800});
+
+	// $("img#cleanr").hover(function(){
+	//     $(this).attr("src", function(index, attr){
+	//         return attr.replace("1.jpg", "2.jpg");
+	//     });
+	// }, function(){
+	//     $(this).attr("src", function(index, attr){
+	//         return attr.replace("2.jpg", "1.jpg");
+	//     });
+	// });
 });
